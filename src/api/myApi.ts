8d819c195f61d0787b6d6a3b170ec3006fbf5929
longnguyencodegym <longnguyen.tourism@gmail.com>
2023-12-01 16:11:26 +0700

    fix condition run resAction

--- src/api/myApi.ts
+++ src/api/myApi.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-const myAction = async (data: AxiosRequestConfig) => {
+const myResponse = async (data: AxiosRequestConfig) => {
   const res = await axios(data);
   return res.data;
 };
-export { myAction };
+export { myResponse };
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import moduleAlias from "module-alias";
 import { config } from "dotenv";
 config();
-import { myAction } from "./api/myApi";
+import { myResponse } from "./api/myApi";
 moduleAlias.addAliases({
   "@": __dirname,
 });
@@ -17,36 +17,27 @@ const name = process.env.MYNAME || "Kun";
 app.use(cors());
 app.use(express.json());
 
-const fetchActions = async (body: any): Promise<boolean> => {
+const responseActions = async (body: any): Promise<boolean> => {
   (data.actions as IAction[]).map(async (item) => {
     const { listen_type, from, to } = item;
-    if (body.action === "opened") {
-      if (
-        listen_type === body.action &&
-        body.pull_request.head.ref === from &&
-        body.pull_request.base.ref === to
-      ) {
-        const res = await myAction(item.action);
-        return true;
-      }
-    } else if (body.action === "closed") {
-      if (
-        !body.pull_request.merged &&
-        listen_type === "closed" &&
-        body.pull_request.head.ref === from &&
-        body.pull_request.base.ref === to
-      ) {
-        const res = await myAction(item.action);
-        return true;
-      }
-      if (
-        body.pull_request.merged &&
-        listen_type === "merged" &&
-        body.pull_request.head.ref === from &&
-        body.pull_request.base.ref === to
-      ) {
-        const res = await myAction(item.action);
-        return true;
+    if (
+      body.pull_request.head.ref === from &&
+      body.pull_request.base.ref === to
+    ) {
+      if (body.action === "opened") {
+        if (listen_type === body.action) {
+          await myResponse(item.action);
+          return true;
+        }
+      } else if (body.action === "closed") {
+        if (!body.pull_request.merged && listen_type === "closed") {
+          await myResponse(item.action);
+          return true;
+        }
+        if (body.pull_request.merged && listen_type === "merged") {
+          await myResponse(item.action);
+          return true;
+        }
       }
     }
   });
@@ -60,8 +51,8 @@ app.get("/", (_req: Request, res: Response) => {
 });
 app.post("/", async (req: Request, res: Response) => {
   try {
-    const response = await fetchActions(req.body);
-    if (response) {
+    const data = await responseActions(req.body);
+    if (data) {
       return res.json({
         message: RUN_ACTION_SUCCESS.msg,
       });
