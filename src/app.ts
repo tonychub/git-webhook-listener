c2bdd18b79d580851b2aec413906e7425bd755e6
longnguyencodegym <longnguyen.tourism@gmail.com>
2023-12-01 09:04:32 +0700

    listen json

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import moduleAlias from "module-alias";
+import { myAction, myFetch } from "./api/myApi";
 moduleAlias.addAliases({
   "@": __dirname,
 });
@@ -7,6 +8,19 @@ import cors from "cors";
 
 import { config } from "dotenv";
 config();
+type IData = {
+  listen_type: string;
+  from: string;
+  to: string;
+  actionGet: {
+    method: string;
+    url: string;
+  };
+  actionPost: {
+    method: string;
+    url: string;
+  };
+};
 
 const port = process.env.PORT || 9999;
 
@@ -14,16 +28,25 @@ const app: Application = express();
 const name = process.env.MYNAME || "Kun";
 app.use(cors());
 app.use(express.json());
+
+const fetchActions = async (body: any) => {
+  const actionData = await myFetch("/actions", "GET");
+
+  (actionData as IData[]).map(async (item) => {
+    if (item.listen_type === body.action) {
+      const res = await myAction(item.actionGet.method, item.actionGet.url);
+      myAction(item.actionPost.method, item.actionPost.url, res.data);
+    }
+  });
+};
+
 app.get("/", (_req: Request, res: Response) => {
   res.json({
     message: "Hello " + name,
   });
 });
 app.post("/", (req: Request, res: Response) => {
-  console.log("data", req.body);
-  res.json({
-    message: "data" + req,
-  });
+  fetchActions(req.body);
 });
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
