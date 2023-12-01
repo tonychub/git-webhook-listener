6dde665a9d3b8eaeb5af769f832f0b2e22f6a2eb
longnguyencodegym <longnguyen.tourism@gmail.com>
2023-12-01 15:22:34 +0700

    fix-response+3 status

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import express, { Application, Request, Response } from "express";
 import data from "./example.json";
 import cors from "cors";
 import { IAction } from "./type";
+import { RUN_ACTION_ERRORS, RUN_ACTION_SUCCESS } from "./constants";
 
 const port = process.env.PORT || 9999;
 const app: Application = express();
@@ -16,17 +17,40 @@ const name = process.env.MYNAME || "Kun";
 app.use(cors());
 app.use(express.json());
 
-const fetchActions = async (body: any) => {
+const fetchActions = async (body: any): Promise<boolean> => {
   (data.actions as IAction[]).map(async (item) => {
     const { listen_type, from, to } = item;
-    if (
-      listen_type === body.action &&
-      body.pull_request.head.ref === from &&
-      body.pull_request.base.ref === to
-    ) {
-      const res = await myAction(item.action);
+    if (body.action === "opened") {
+      if (
+        listen_type === body.action &&
+        body.pull_request.head.ref === from &&
+        body.pull_request.base.ref === to
+      ) {
+        const res = await myAction(item.action);
+        return true;
+      }
+    } else if (body.action === "closed") {
+      if (
+        !body.pull_request.merged &&
+        listen_type === "closed" &&
+        body.pull_request.head.ref === from &&
+        body.pull_request.base.ref === to
+      ) {
+        const res = await myAction(item.action);
+        return true;
+      }
+      if (
+        body.pull_request.merged &&
+        listen_type === "merged" &&
+        body.pull_request.head.ref === from &&
+        body.pull_request.base.ref === to
+      ) {
+        const res = await myAction(item.action);
+        return true;
+      }
     }
   });
+  return false;
 };
 
 app.get("/", (_req: Request, res: Response) => {
@@ -34,8 +58,23 @@ app.get("/", (_req: Request, res: Response) => {
     message: "Hello " + name,
   });
 });
-app.post("/", (req: Request, res: Response) => {
-  fetchActions(req.body);
+app.post("/", async (req: Request, res: Response) => {
+  try {
+    const response = await fetchActions(req.body);
+    if (response) {
+      return res.json({
+        message: RUN_ACTION_SUCCESS.msg,
+      });
+    } else {
+      res.status(RUN_ACTION_ERRORS.code).json({
+        message: RUN_ACTION_ERRORS.msg,
+      });
+    }
+  } catch (error) {
+    res.status(RUN_ACTION_ERRORS.code).json({
+      message: RUN_ACTION_ERRORS.msg,
+    });
+  }
 });
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
